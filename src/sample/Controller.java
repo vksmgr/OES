44f6611201593bd9db77bb41a87b18fbec7e2f9f
vksmgr <cseghrce@gmail.com>
2017-01-28 15:04:33 +0530

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+
+    @FXML
+    private Button clickMe;
+    @FXML
+    private Label welcomeLable;
+
+    public void clicked(ActionEvent actionEvent) {
+
+        welcomeLable.setText("Welcome To  First program");
+    }
+}
